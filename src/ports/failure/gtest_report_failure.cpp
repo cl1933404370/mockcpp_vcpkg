0ee7b4999703bb1178d6bbe3dd4c5a17b6dcac04
cl1933404370 <cl19334074370@outlook.com>
2025-03-30 03:12:15 +0800

    Update CMakeLists.txt to clear MOCKCPP_XUNIT for MSVC and remove unnecessary GTEST_LINKED_AS_DLL definition in gtest_report_failure.cpp

--- src/ports/failure/gtest_report_failure.cpp
+++ src/ports/failure/gtest_report_failure.cpp
@@ -15,9 +15,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ***/
-#ifndef GTEST_LINKED_AS_DLL
-#define GTEST_LINKED_AS_DLL
-#endif
 #include <mockcpp/Asserter.h>
 #include <gtest/gtest.h>
 
